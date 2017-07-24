087d0eb4205262ce8310f468ed8975a201db81dd
Vladimir Kochenov <kochenov.vl@gmail.com>
2017-07-24 19:37:25 +0300

    исправил отображение активной ссылки в главном меню

--- app/blocks/main-menu/main-menu.js
+++ app/blocks/main-menu/main-menu.js
@@ -3,6 +3,6 @@ var elem = document.querySelectorAll('.main-menu__link');
 
 for(var i = 0; i < elem.length; i++) {
 	if(elem[i].getAttribute('href') == window.location.pathname) {
-		elem[i].className = 'main-menu__link main-menu__link_active';
+		elem[i].classList.add('main-menu__link_active');
 	}
 }
