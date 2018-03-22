2d86dabc4c36e1cdb0eb4e9505599847779a0611
Vladimir Kochenov <kochenov.vl@gmail.com>
2018-03-22 09:07:39 +0300

    Исправил запись в package.json для Gulp 4

--- tasks/images.js
+++ tasks/images.js
@@ -2,16 +2,19 @@
 
 const gulp = require('gulp');
 const newer = require('gulp-newer');
+const gulpIf = require('gulp-if');
 const image = require('gulp-image');
 
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 module.exports = () => (
 	gulp.src('app/assets/img/**/*.*')
 	.pipe(newer('public/assets/img'))
-	.pipe(image({
+	.pipe(gulpIf(!isDevelopment, image({
       mozjpeg: false,
       jpegoptim: false,
       jpegRecompress: true
-    }))
+    })))
+
 	.pipe(gulp.dest('public/assets/img'))
 )
