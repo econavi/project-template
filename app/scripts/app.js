0bd69ceeec543241ad142d42f4b8adc78dfb7574
Vladimir Kochenov <kochenov.vl@gmail.com>
2018-06-28 14:59:24 +0300

    мелкие правки по структуре demo-lib

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,8 @@
-import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
-import mainMenu from './../blocks/main-menu/main-menu';
-import textarea from './../blocks/textarea/textarea';
+// import svg4everybody from 'svg4everybody';
+// import $ from 'jquery';
+// import mainMenu from './../blocks/main-menu/main-menu';
+// import textarea from './../blocks/textarea/textarea';
 
-window.onload = () => {
-	svg4everybody();
-};
+// window.onload = () => {
+// 	svg4everybody();
+// };
